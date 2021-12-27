cc8dbc39d1cdec0be74cdbf721534181b0fd8446
Waylon Jepsen <waylonjepsen1@gmail.com>
2021-12-27 13:27:35 -0700

    successful interaction with WETH contract on kovan

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -1,6 +1,16 @@
+from brownie import interface, config, network
+from scripts.tooling import get_account
+
+
 def get_weth():
+    account = get_account(id="testing_account")
+    print(network.show_active())
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    print("Recieved 0.1 Weth")
+    return tx
+
     # need abi and address or interface
-    pass
 
 
 def main():
---
+++ scripts/tooling.py
@@ -0,0 +1,17 @@
+from brownie import network, accounts, config
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "mainnet-fork"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        print(accounts[0].balance())
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    if network.show_active() in config["networks"]:
+        account = accounts.add(config["wallets"]["from_key"])
+        return account
+    return None
