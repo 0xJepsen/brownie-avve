43660be12441dfe76808e11f830414e6fe8bdf79
Waylon Jepsen <waylonjepsen1@gmail.com>
2021-12-27 13:05:44 -0700

    Weth interface

---
+++ scripts/avve_barrow.py
@@ -0,0 +1,5 @@
+from brownie import accounts
+
+
+def main():
+    pass
---
+++ scripts/get_weth.py
@@ -0,0 +1,10 @@
+def get_weth():
+    # need abi and address or interface
+    pass
+
+
+def main():
+    get_weth()
+
+
+main()
