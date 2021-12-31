97e7dc080bdec62e49a693e800998994ab9eda45
Waylon Jepsen <waylonjepsen1@gmail.com>
2021-12-31 12:14:13 -0700

    approve erc20

--- scripts/aave_barrow.py
+++ scripts/aave_barrow.py
@@ -5,15 +5,19 @@ from brownie import (
 )
 from scripts.tooling import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
 
+
+AMOUNT = Web3.toWei(0.1, "ether")
 # id="testing_account"
 def main():
     account = get_account()
-    erc20_weth_address = config["networks"][network.show_active()]["weth_token"]
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
     lending_pool = get_lending_pool()
     print(lending_pool)
+    approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
 
 
 def get_lending_pool():
@@ -23,5 +27,11 @@ def get_lending_pool():
     lending_pool_address = lending_pool_addresses_provider.getLendingPool()
     lending_pool = interface.ILendingPool(lending_pool_address)
     return lending_pool
-    # abi
-    # address
+
+
+def approve_erc20(amount, spender_address, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender_address, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
