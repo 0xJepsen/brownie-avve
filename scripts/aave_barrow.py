0326a145a61915ddd68f0d29a439bd3164017708
Waylon Jepsen <waylonjepsen1@gmail.com>
2021-12-31 12:01:07 -0700

    getlendingpool

--- scripts/aave_barrow.py
+++ scripts/aave_barrow.py
@@ -1,5 +1,8 @@
-from brownie.network import account
-from brownie import config, network
+from brownie import (
+    config,
+    network,
+    interface,
+)
 from scripts.tooling import get_account
 from scripts.get_weth import get_weth
 
@@ -8,4 +11,17 @@ def main():
     account = get_account()
     erc20_weth_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
-        tx = get_weth()
+        get_weth()
+    lending_pool = get_lending_pool()
+    print(lending_pool)
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+    # abi
+    # address
